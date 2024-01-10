5515a710a0f5cd537691539c2a2dc78a45ed6123
엉히킴 <cin05189@naver.com>
2024-01-10 15:36:50 +0900

    feat: scheduler remains

--- vms/src/main/java/com/example/vms/scheduler/controller/SchedulerController.java
+++ vms/src/main/java/com/example/vms/scheduler/controller/SchedulerController.java
@@ -1,5 +1,13 @@
 package com.example.vms.scheduler.controller;
 
-public class SchedulerController {
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class SchedulerController {
+	
+	@Scheduled(cron = "*/20 * * * * *") 
+    public void second100() {
+        System.out.println("안녕 히원아?");
+    } 
 }
