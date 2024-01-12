97bb1462ceb9417a21e7659d9b35f2602f9e07c3
엉히킴 <cin05189@naver.com>
2024-01-12 14:31:03 +0900

    feat: 연차 촉진 메일 내역 저장

--- vms/src/main/java/com/example/vms/scheduler/service/ISchedulerService.java
+++ vms/src/main/java/com/example/vms/scheduler/service/ISchedulerService.java
@@ -1,9 +1,12 @@
 package com.example.vms.scheduler.service;
 
+import com.example.vms.scheduler.model.Scheduler;
 import com.example.vms.scheduler.model.SchedulerResult;
 
 public interface ISchedulerService {
 	void certificateScheduler();
+	void saveScheduler(Scheduler scheduler);
+	public int maxSchedulerId();
 	
 	SchedulerResult[] searchSchedulers(
 //		Date startDate,
